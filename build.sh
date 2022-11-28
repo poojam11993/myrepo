6009781ca9914912a6a3760b57f8edde58964c6a
poojam11993 <118235909+poojam11993@users.noreply.github.com>
2022-11-29 00:12:07 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf hello-world-war
+git clone https://github.com/poojam11993/myrepo.git
+cd hello-world-war
+mvn package
+sudo cp -r target/hello-world-war-1.0.0.war /opt/tomcat-10.0.27/webapps
+sh /opt/apache-tomcat-10.0.27/bin/shutdown.sh
+sleep 3
+sh /opt/apache-tomcat-10.0.27/bin/startup.sh
+exec bash
