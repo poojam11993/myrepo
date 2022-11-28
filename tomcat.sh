fcd2ef80411b51eff46d15f4e38a41867875c13a
poojam11993 <118235909+poojam11993@users.noreply.github.com>
2022-11-28 23:52:53 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -12,7 +12,7 @@ sudo sh /opt/tomcat/bin/shutdown.sh
 sleep 3
 sudo sh /opt/tomcat/bin/startup.sh
 sudo rm -rf conf-and-webapps-file
-sudo git clone https://github.com/poojam11993/myrepo.git
+sudo git clone https://github.com/syedwaliuddin/conf-and-webapps-file.git
 sudo rm -rf /opt/tomcat/conf/tomcat-users.xml
 sudo cp conf-and-webapps-file/tomcat-users.xml /opt/tomcat/conf/
 sudo sh /opt/tomcat/bin/shutdown.sh
@@ -23,5 +23,5 @@ sudo cp conf-and-webapps-file/context.xml /opt/tomcat/webapps/manager/META-INF/
 sudo rm -rf /opt/tomcat/webapps/host-manager/META-INF/context.xml
 sudo cp conf-and-webapps-file/contexthm.xml /opt/tomcat/webapps/host-manager/META-INF/
 sudo sh /opt/tomcat/bin/shutdown.sh
-sleep 3
+sleep 2
 sudo sh /opt/tomcat/bin/startup.sh
