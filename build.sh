cc7de84d254226346df6f32e14486ea469690f0d
poojam11993 <118235909+poojam11993@users.noreply.github.com>
2022-11-29 19:36:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf hello-world-war
 git clone https://github.com/poojam11993/myrepo.git
 cd hello-world-war
 mvn package
-sudo cp -r target/hello-world-war-1.0.0.war /opt/tomcat-10.0.27/webapps
+sudo cp -R target/hello-world-war-1.0.0.war /opt/tomcat-10.0.27/webapps
 sh /opt/apache-tomcat-10.0.27/bin/shutdown.sh
 sleep 3
 sh /opt/apache-tomcat-10.0.27/bin/startup.sh
